d978a08a6664b6e802ebafb24512dc487fd1bb07
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-19 08:50:31 +0700

    Add pages

---
+++ src/pages/auth/logout.tsx
@@ -0,0 +1,9 @@
+const Logout = () => {
+  return (
+    <>
+      <h1>Logout Section</h1>
+    </>
+  );
+};
+
+export default Logout;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,18 @@
+import Title from "@/components/Title";
+import Footer from "@/components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Title title="home"/>
+      <main>
+        <section>
+          <h1>Home section</h1>
+        </section>
+      </main>
+      <Footer/>
+    </>
+  );
+};
+
+export default Home;
