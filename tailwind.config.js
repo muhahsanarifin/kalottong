3b2a597a7733975431bd3f85bd30ef1a2f045b98
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-03-21 09:18:47 +0700

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -9,7 +10,50 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "red=orange": "#ff5f26",
+        "cyan-blue": "#293038",
+        red: {
+          50: "#fef2f2",
+          100: "#fee2e2",
+          200: "#fecaca",
+          300: "#fca5a5",
+          400: "#f87171",
+          500: "#ef4444",
+          600: "#dc2626",
+          700: "#b91c1c",
+          800: "#991b1b",
+          900: "#7f1d1d",
+        },
+      },
+    },
+    screens: {
+      "3xl": { min: "1536px" },
+      // => @media (min-width: 1536px)
+
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px)
+
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px)
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px)
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px)
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px)
+
+      xs: { max: "475px" },
+      // => @media (max-width: 475px)
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("flowbite/plugin"),
+    require("@tailwindcss/forms")({ strategy: "base" }),
+  ],
 };
