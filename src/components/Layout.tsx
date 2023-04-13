1790646806323438b054c1348449c9e3dbeaf931
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-14 06:13:26 +0700

    Updated Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { TitleProps } from "../utils/types/title";
+import { TitleProps } from "../utils/types/titleType";
 
 const TitleHome = ({ children }: TitleProps) => {
   return (
@@ -27,4 +27,30 @@ const TitleProfile = ({ children }: TitleProps) => {
   );
 };
 
-export { TitleHome, TitleProfile };
+const TitleRegister = ({ children }: TitleProps) => {
+  return (
+    <>
+      <Head>
+        <title>kalottong | Register</title>
+        <meta name="description" content="Task management" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      {children}
+    </>
+  );
+};
+
+const TitleForgetPassword = ({ children }: TitleProps) => {
+  return (
+    <>
+      <Head>
+        <title>kalottong | Forget Password</title>
+        <meta name="description" content="Task management" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      {children}
+    </>
+  );
+};
+
+export { TitleHome, TitleProfile, TitleRegister, TitleForgetPassword };
