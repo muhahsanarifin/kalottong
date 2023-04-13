534eef0dac6433092a7580f0dc8a8587e5d9f9be
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-14 06:06:23 +0700

    Updated Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Button, Modal, Label, Checkbox } from "flowbite-react";
 import { AuthModalProps } from "@/utils/types/modalType";
+import Link from "next/link";
+import { login } from "@/utils/api/auth";
+import { SpinnerLoader, ErrorMessage } from "./Feed";
+import { setCookie } from "cookies-next";
+import {
+  setItemLocalStorage,
+  getItemLocalStorage,
+} from "@/utils/storage/localStorage";
 
 export const AuthModal = ({ onShow, onSetClose }: AuthModalProps) => {
+  const [errorResponse, setErrorResponse] = useState<any>("");
+  const [loader, setLoader] = useState<boolean>(false);
+  const [remember, setRemember] = useState<boolean>(false);
+  const [displayEmail, setDisplayEmail] = useState<any>("");
+  const email = useRef<any>("");
+  const password = useRef<any>("");
+
+  const handleLogin = async () => {
+    if (email.current.value === "") {
+      return setErrorResponse("Required email");
+    }
+
+    if (password.current.value === "") {
+      return setErrorResponse("Required password");
+    }
+
+    try {
+      setLoader(true);
+      const response = await login({
+        email: email.current.value,
+        password: password.current.value,
+      });
+
+      console.log("Another response:", response);
+      if (response.status === 200) {
+        setCookie("data-user", JSON.stringify(response.data.data));
+        if (remember === true) {
+          setItemLocalStorage("email-user", response.data.data?.email);
+        }
+      }
+    } catch (error: any) {
+      setErrorResponse(error.response.data.msg);
+    } finally {
+      setLoader(false);
+    }
+  };
+
+  const sample = () => {
+    setRemember(!remember);
+  };
+
+  useEffect(() => {
+    const coba = getItemLocalStorage("email-user");
+    setDisplayEmail(coba);
+  }, []);
+
+  const toClear = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
   return (
     <>
       <Modal
@@ -24,12 +83,16 @@ export const AuthModal = ({ onShow, onSetClose }: AuthModalProps) => {
                 <Label htmlFor="email" value="Your email" />
               </div>
               <input
+                ref={email}
                 name="email"
                 id="email"
                 type="email"
-                required={true}
                 placeholder="name@company.com"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-orange focus:border-red-orange block w-full p-2.5"
+                className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 ${
+                  errorResponse === "Email is not registered" &&
+                  "border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500"
+                }`}
+                value={displayEmail ? displayEmail : null}
               />
             </div>
             <div>
@@ -37,41 +100,64 @@ export const AuthModal = ({ onShow, onSetClose }: AuthModalProps) => {
                 <Label htmlFor="password" value="Your password" />
               </div>
               <input
+                ref={password}
                 name="password"
                 id="password"
                 type="password"
-                required={true}
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-orange focus:border-red-orange block w-full p-2.5"
+                className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 ${
+                  errorResponse === "Wrong password" &&
+                  "border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500"
+                }`}
               />
             </div>
+            <div className="flex justify-center">
+              {errorResponse ? <ErrorMessage msg={errorResponse} /> : null}
+            </div>
             <div className="flex justify-between">
-              <div className="flex items-center gap-2">
-                <Checkbox
-                  id="remember"
-                  className="text-red-orange focus:ring-red-orange"
-                />
-                <Label htmlFor="remember">Remember me</Label>
-              </div>
-              <a
-                href="/modal"
+              {displayEmail ? (
+                <div className="flex items-center gap-2">
+                  <button
+                    className="text-[12px] font-bold hover:text-gray-900"
+                    onClick={toClear}
+                  >
+                    Clear email
+                  </button>
+                </div>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <Checkbox id="remember" onClick={sample} />
+                  <Label htmlFor="remember">Remember me</Label>
+                </div>
+              )}
+              <Link
+                href="/password/reset"
                 className="text-sm text-red-orange hover:underline"
               >
                 Forget Password?
-              </a>
+              </Link>
             </div>
             <div className="w-full">
               <Button
-                className="bg-red-orange hover:bg-red-orange-dark focus:ring-4 focus:ring-red-orange-light"
-                onClick={() => console.log("Test")}
+                className={`bg-red-orange hover:bg-red-orange-dark focus:ring-4 focus:ring-red-orange-light w-full ${
+                  loader && "cursor-not-allowed"
+                }`}
+                onClick={handleLogin}
               >
-                Log in to your account
+                {loader ? (
+                  <SpinnerLoader />
+                ) : (
+                  <span>Log in to your account</span>
+                )}
               </Button>
             </div>
             <div className="text-sm font-medium text-gray-500">
               Not registered?
-              <a href="/modal" className="text-red-orange hover:underline">
+              <Link
+                href="/auth/register"
+                className="text-red-orange hover:underline"
+              >
                 Create account
-              </a>
+              </Link>
             </div>
           </div>
         </Modal.Body>
