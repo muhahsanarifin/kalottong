84c91e8b078248123c1f125ffed3d83041abe721
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-14 06:11:46 +0700

    Updated Forms.tsx

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Label, TextInput, Checkbox, Radio } from "flowbite-react";
 import { FormProps } from "@/utils/types/formType";
 import Link from "next/link";
+import { RegulerButton } from "./Button";
 
 const SortingDropDown = ({ onHidden }: FormProps) => {
   return (
@@ -61,6 +62,9 @@ const SortingDropDown = ({ onHidden }: FormProps) => {
 };
 
 const RegisterForm = () => {
+  const [email, setEmail] = useState("");
+
+  console.log("email:", email);
   return (
     <>
       <form className="flex flex-col gap-4 w-1/2">
@@ -74,6 +78,7 @@ const RegisterForm = () => {
             placeholder="name@kalottong.com"
             required={true}
             shadow={true}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div>
@@ -100,7 +105,9 @@ const RegisterForm = () => {
         </div>
         <div>
           <fieldset className="flex gap-4" id="radio">
-            <legend className="text-[14px] font-medium my-1">Choose gender</legend>
+            <legend className="text-[14px] font-medium my-1">
+              Choose gender
+            </legend>
             <div className="flex items-center gap-2">
               <Radio id="man" name="gender" value="man" />
               <Label htmlFor="man">Man</Label>
@@ -131,4 +138,41 @@ const RegisterForm = () => {
   );
 };
 
-export { SortingDropDown, RegisterForm };
+const ResetPasswordForm = () => {
+  return (
+    <>
+      <label
+        htmlFor="input-group-1"
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-1/2 mx-auto"
+      >
+        Your Email
+      </label>
+      <div className="relative mb-6 w-1/2 mx-auto flex gap-x-2">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="input-group-1"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="name@flowbite.com"
+        />
+        <RegulerButton
+          title="Submit"
+          onsSetAction={() => console.log("Test")}
+        />
+      </div>
+    </>
+  );
+};
+
+export { SortingDropDown, RegisterForm, ResetPasswordForm };
