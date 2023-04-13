7839766219bc1ba9f3c7ba3a66921300320e60b0
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-14 06:13:41 +0700

    Updated Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,26 +1,32 @@
-import React, { useState } from "react";
-import { AddTaskButton, SortingButton } from "./Button";
-import { SortingDropDown } from "./Forms";
+import React, { useState, useEffect } from "react";
 import { useToggle } from "usehooks-ts";
+import { AddTaskButton, DoneTaskButton, SortingButton } from "./Button";
+import { SortingDropDown } from "./Forms";
+import { Task } from "./Task";
+import { InputTask } from "./Input";
 
 const Main = () => {
   const [hidden, toggle] = useToggle();
   // console.log("Result: ", hidden);
+  const [hiddenAddTask, setHiddenAddTask] = useState(false);
+  const [hiddenDoneTask, setHiddenDoneTask] = useState(false);
+
+  // const [arr, setArr] = useState(new Array(10));
 
   return (
     <>
-      <main className="border-solid border-2 h-screen flex flex-col">
-        <div className="border-green-900 border-solid border-2 w-[80%] h-[100%] mx-auto p-[2.5rem]">
-          <div className="border-2 border-solid border-yellow-800 h-[100%]">
+      <main className="min-h-screen flex flex-col">
+        <div className="shadow-[0_16px_90px_rgba(19,7,52,0.08)] rounded-[12px] w-[80%] min-h-[100%] mx-auto p-[2.5rem]">
+          <div className="h-[100%] flex flex-col gap-y-2">
             {/* Intro & Add Task Button  section */}
-            <section className="border-2 border-solid border-blue-600 flex items-center">
+            <section className="flex items-center">
               {/* Introduction */}
               <div className="flex flex-col">
                 <p className="text-red-orange font-medium text-[16px]">
                   MY TASK
                 </p>
                 <h1 className="text-[24px] font-medium text-cyan-blue">
-                  TO DO List
+                  To Do List
                 </h1>
                 <p className="text-[#7A7F83] font-normal text-[16px]">
                   Buat list tugas harian saya
@@ -28,22 +34,50 @@ const Main = () => {
               </div>
               {/* Button */}
               <div className="ml-auto">
-                <AddTaskButton />
+                <AddTaskButton
+                  onSetToggle={() => setHiddenAddTask(!hiddenAddTask)}
+                />
               </div>
             </section>
 
             {/* Sort  & Button section */}
-            <section className="border-2 border-solid border-red-600 flex items-center">
+            <section className="flex items-center">
               <p className="text-[#7A7F83] font-medium text-[16px]">Sort By</p>
               {/* Button */}
-              <div className="ml-auto border-red-800 relative">
-                <SortingButton onSetToggle={toggle} />
+              <div className="ml-auto relative">
+                <SortingButton onSetToggle={toggle} onHidden={hidden} />
                 <SortingDropDown onHidden={hidden} />
               </div>
             </section>
 
-            {/* Task list section*/}
-            <section></section>
+            <section className={!hiddenAddTask ? "hidden" : "p-2 bg-[#F5F5F5]"}>
+              <InputTask />
+            </section>
+
+            {/* On-going & Done section*/}
+            <section>
+              {/* On-going task */}
+              <div className="p-2">
+                <Task tasks={new Array(5)} />
+              </div>
+
+              {/* Done task */}
+              <div className="border-t-2 border-t-solid border-t-[#CCCED2] pt-2">
+                <div className="flex items-center gap-x-2">
+                  <DoneTaskButton
+                    onSetToggle={() => setHiddenDoneTask(!hiddenDoneTask)}
+                    onHidden={hiddenDoneTask}
+                  />
+                  <span className="flex items-center gap-x-2">
+                    <p className="text-[#7A7F83] font-[500]">Terselesaikan</p>
+                    <p className="text-[#7A7F83] font-[500]">(3 Tugas)</p>
+                  </span>
+                </div>
+              </div>
+              <div className={!hiddenDoneTask ? "hidden" : "p-2"}>
+                <Task tasks={new Array(3)} />
+              </div>
+            </section>
           </div>
         </div>
       </main>
