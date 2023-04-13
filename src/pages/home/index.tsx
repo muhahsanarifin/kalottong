6205eb751a24ddf66075b8f2d59f8c880ff34f3b
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-14 06:15:48 +0700

    Updated index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { TitleHome } from "@/components/Layout";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Main from "@/components/main";
+import Main from "@/components/Main";
 
 const Home = () => {
   return (
