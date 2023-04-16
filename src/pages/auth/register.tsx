d4c8570ba57d7a9964499afcb015aa510dc583b3
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-17 00:14:40 +0700

    Updated register.tsx

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { RegisterForm } from "@/components/Forms";
 import { TitleRegister } from "@/components/Layout";
+import Breadcrumb from "@/components/Breadcrumb";
 
 const Register = () => {
   return (
     <>
       <TitleRegister>
+        <section className="flex">
+          <Breadcrumb page="Register"/>
+        </section>
         <section className="flex flex-col items-center">
           <h3 className="text-2xl font-bold text-gray-900">
             <span className="text-red-orange">kalottong</span> member
