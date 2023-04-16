aeab0cde2b9593850c5f306ea815e91928211dfd
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-17 00:51:16 +0700

    Updated reset.tsx

--- src/pages/password/reset.tsx
+++ src/pages/password/reset.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { ResetPasswordForm } from "@/components/Forms";
-import { BackButton } from "@/components/Button";
 import { TitleForgetPassword } from "@/components/Layout";
+import Breadcrumb from "@/components/Breadcrumb";
+import Footer from "@/components/Footer";
 
 const ResetPassword = () => {
   return (
     <>
       <TitleForgetPassword>
-        <section className="h-[50vh] flex flex-col justify-center">
+        <header>
+          <Breadcrumb page="Forgot Password" />
+        </header>
+        <main className="h-[90vh] flex flex-col justify-center gap-y-2">
           <h3 className=" text-[24px] font-[600] mx-auto">
             kalot<span className="text-red-orange">tong.</span>
           </h3>
-          <ResetPasswordForm />
-          <div className="flex justify-center">
-            <BackButton onRoute="/home" title="Home" />
-          </div>
-        </section>
+          <section className="w-[50%] mx-auto">
+            <ResetPasswordForm />
+          </section>
+        </main>
+        <Footer/>
       </TitleForgetPassword>
     </>
   );
