f06a94961964d8eaca93dd920c0a5796a3adc329
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-20 07:45:52 +0700

    Updated input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import Menu from "../assets/icons/menu.png"
-import Calender from "../assets/icons/calendar.png"
+import Menu from "../assets/icons/menu.png";
+import Calender from "../assets/icons/calendar.png";
 
 const InputTask: React.FC = () => {
   return (
@@ -61,4 +61,36 @@ const InputTask: React.FC = () => {
   );
 };
 
-export { InputTask };
+const InputProfile: React.FC<{
+  onType: string;
+  onTitle: string;
+  setOnChange?: any;
+  onSetValue?: any;
+  onId?: string;
+  onHtmlFor?: string;
+  onDisable: boolean
+}> = ({ onType, onTitle, onId, onHtmlFor, setOnChange, onSetValue, onDisable }) => {
+  return (
+    <>
+      <input
+        type={onType}
+        id={onId}
+        className={`block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+          onDisable && "cursor-not-allowed"
+        }`}
+        placeholder=" "
+        onChange={setOnChange}
+        value={onSetValue}
+        disabled={onDisable}
+      />
+      <label
+        htmlFor={onHtmlFor}
+        className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+      >
+        {onTitle}
+      </label>
+    </>
+  );
+};
+
+export { InputTask, InputProfile };
