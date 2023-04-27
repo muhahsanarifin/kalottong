e1111decbd2a08416ba25ef4153afb5687738fde
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-28 01:43:02 +0700

    Updated auth.ts

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,10 +1,10 @@
 import Axios from "axios";
 
-import { TokenProps, BodyProps } from "../types/apiType";
+import { BodyArg } from "../types/apiType";
 
 const BASE_URL = process.env.NEXT_PUBLIC_KALOTTONG_BACK_END;
 
-const config = (accessToken: TokenProps) => {
+const config = (accessToken: any) => {
   return {
     headers: {
       Authorization: `Bearer ${accessToken}`,
@@ -12,11 +12,10 @@ const config = (accessToken: TokenProps) => {
   };
 };
 
-export const login = ({ ...body }: BodyProps) =>
-  Axios.post(`${BASE_URL}/auth/login`, body);
+export const login = (body: BodyArg) => Axios.post(`${BASE_URL}/auth/login`, body);
 
 export const logout = (accessToken: any) =>
   Axios.delete(`${BASE_URL}/auth/logout`, config(accessToken));
 
-export const register = ({ ...body }: BodyProps) =>
+export const register = (body: BodyArg) =>
   Axios.post(`${BASE_URL}/auth/register`, body);
