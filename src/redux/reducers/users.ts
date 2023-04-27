0c5ccad6ea80f43f14f0b190586b217aa2434284
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-04-28 01:29:53 +0700

    Updated user.ts

--- src/redux/reducers/users.ts
+++ src/redux/reducers/users.ts
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import { UserState } from "@/utils/types/reduxType";
+import { UserState, ArgProfileThunk } from "@/utils/types/reduxType";
+import { getCookie } from "cookies-next";
 import {
   retriveProfile,
   editProfile,
@@ -9,68 +10,120 @@ import {
 } from "@/utils/api/users";
 
 const initialState: UserState = {
-  retriveProfile: [],
-  editProfile: [],
-  uploadImageProfile: [],
-  editNoTelp: [],
-  isLoading: false,
-  isFulfilled: false,
-  isRejected: false,
-  err: null,
+  retriveProfile: {
+    isLoading: false,
+    isFulfilled: false,
+    isRejected: false,
+    data: null,
+    err: null,
+  },
+  editProfile: {
+    isLoading: false,
+    isFulfilled: false,
+    isRejected: false,
+    data: null,
+    err: null,
+  },
+  uploadImageProfile: {
+    isLoading: false,
+    isFulfilled: false,
+    isRejected: false,
+    data: null,
+    err: null,
+  },
+  editNoTelp: {
+    isLoading: false,
+    isFulfilled: false,
+    isRejected: false,
+    data: null,
+    err: null,
+  },
 };
 
 const retriveProfileThunk = createAsyncThunk(
   "users/profile",
-  async (accessToken?: any) => {
+  async ({ cbPending, cbFulfilled, cbFinally }: ArgProfileThunk) => {
     try {
-      const response = await retriveProfile(accessToken);
-      // console.log("Response retrive data profile:", response.data.data[0]);
+      typeof cbPending === "function" && cbPending();
+      const response = await retriveProfile(getCookie("token"));
+      typeof cbFulfilled === "function" && cbFulfilled(response.data);
       return response.data?.data[0];
-    } catch (error) {
-      console.error(error);
-      return error;
+    } catch (error: any) {
+      if (error.response) {
+        console.error(error.response.data?.msg);
+        throw error.response.data?.msg;
+      } else {
+        console.error(error);
+        throw error;
+      }
+    } finally {
+      typeof cbFinally === "function" && cbFinally();
     }
   }
 );
 
 const editProfileThunk = createAsyncThunk(
   "users/profile/edit",
-  async (accessToken: any, body: any) => {
+  async ({ body, cbPending, cbFulfilled, cbFinally }: ArgProfileThunk) => {
     try {
-      const response = await editProfile(accessToken, body);
-      // console.log("Response edit profile:", response);
+      typeof cbPending === "function" && cbPending();
+      const response = await editProfile(getCookie("token"), body);
+      typeof cbFulfilled === "function" && cbFulfilled(response.data);
       return response.data;
-    } catch (error) {
-      console.error(error);
-      return error;
+    } catch (error: any) {
+      if (error.response) {
+        console.error(error.response.data?.msg);
+        throw error.response.data?.msg;
+      } else {
+        console.error(error);
+        throw error;
+      }
+    } finally {
+      typeof cbFinally === "function" && cbFinally();
     }
   }
 );
 
 const uploadImageProfileThunk = createAsyncThunk(
   "users/profile/upload",
-  async (accessToken: any, body: any) => {
+  async ({ body, cbPending, cbFulfilled, cbFinally }: ArgProfileThunk) => {
     try {
-      const response = await uploadImageProfile(accessToken, body);
-      // console.log("Response upload image profile:", response);
+      typeof cbPending === "function" && cbPending();
+      const response = await uploadImageProfile(getCookie("token"), body);
+      typeof cbFulfilled === "function" && cbFulfilled(response.data);
       return response.data;
-    } catch (error) {
-      console.error(error);
-      return error;
+    } catch (error: any) {
+      if (error.response) {
+        console.error(error.response.data?.msg);
+        throw error.response.data?.msg;
+      } else {
+        console.error(error);
+        throw error;
+      }
+    } finally {
+      typeof cbFinally === "function" && cbFinally();
     }
   }
 );
 
 const editNoTelpThunk = createAsyncThunk(
   "users/notelp/edit",
-  async (accessToken: any, body: any) => {
+  async ({ body, cbPending, cbFulfilled, cbFinally }: ArgProfileThunk) => {
     try {
-      const response = await editNoTelp(accessToken, body);
-      // console.log("Response edit no telp:", response);
+      typeof cbPending === "function" && cbPending();
+      const response = await editNoTelp(getCookie("token"), body);
+      typeof cbFulfilled === "function" && cbFulfilled(response.data);
       return response.data;
-    } catch (error) {
-      console.error(error);
-      return error;
+    } catch (error: any) {
+      if (error.response) {
+        console.error(error.response.data?.msg);
+        throw error.response.data?.msg;
+      } else {
+        console.error(error);
+        throw error;
+      }
+    } finally {
+      typeof cbFinally === "function" && cbFinally();
     }
   }
 );
@@ -79,23 +132,27 @@ const userSlice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    reset: (prevState) => {
-      return {
-        ...prevState,
-        retriveProfile: [],
-        editProfile: [],
-        uploadImageProfile: [],
-        editNoTelp: [],
-      };
-    },
+    // reset: (prevState) => {
+    //   return {
+    //     ...prevState,
+    //     retriveProfile: null,
+    //     editProfile: null,
+    //     uploadImageProfile: null,
+    //     editNoTelp: null,
+    //   };
+    // },
   },
   extraReducers: (builder) => {
     builder.addCase(retriveProfileThunk.pending, (prevState) => {
       return {
         ...prevState,
-        isLoading: true,
-        isFulfilled: false,
-        isRejected: false,
+        retriveProfile: {
+          isLoading: true,
+          isFulfilled: false,
+          isRejected: false,
+          data: null,
+          err: null,
+        },
       };
     });
     builder.addCase(
@@ -103,31 +160,38 @@ const userSlice = createSlice({
       (prevState, action: PayloadAction<any>) => {
         return {
           ...prevState,
-          isLoading: false,
-          isFulfilled: true,
-          isRejected: false,
-          retriveProfile: action.payload,
+          retriveProfile: {
+            isLoading: false,
+            isFulfilled: true,
+            isRejected: false,
+            data: action.payload,
+            err: null,
+          },
         };
       }
     );
-    builder.addCase(
-      retriveProfileThunk.rejected,
-      (prevState, action: PayloadAction<any>) => {
-        return {
-          ...prevState,
+    builder.addCase(retriveProfileThunk.rejected, (prevState, action) => {
+      return {
+        ...prevState,
+        retriveProfile: {
           isLoading: false,
           isFulfilled: false,
           isRejected: true,
-          err: action.payload,
-        };
-      }
-    );
+          data: { msg: action.error.message },
+          err: action.error.message,
+        },
+      };
+    });
     builder.addCase(editProfileThunk.pending, (prevState) => {
       return {
         ...prevState,
-        isLoading: true,
-        isFulfilled: false,
-        isRejected: false,
+        editProfile: {
+          isLoading: true,
+          isFulfilled: false,
+          isRejected: false,
+          data: null,
+          err: null,
+        },
       };
     });
     builder.addCase(
@@ -135,31 +199,38 @@ const userSlice = createSlice({
       (prevState, action: PayloadAction<any>) => {
         return {
           ...prevState,
-          isLoading: false,
-          isFulfilled: true,
-          isRejected: false,
-          editProfile: action.payload,
+          editProfile: {
+            isLoading: false,
+            isFulfilled: true,
+            isRejected: false,
+            data: action.payload,
+            err: null,
+          },
         };
       }
     );
-    builder.addCase(
-      editProfileThunk.rejected,
-      (prevState, action: PayloadAction<any>) => {
-        return {
-          ...prevState,
+    builder.addCase(editProfileThunk.rejected, (prevState, action) => {
+      return {
+        ...prevState,
+        editProfile: {
           isLoading: false,
           isFulfilled: false,
           isRejected: true,
-          err: action.payload,
-        };
-      }
-    );
+          data: { msg: action.error.message },
+          err: action.error.message,
+        },
+      };
+    });
     builder.addCase(uploadImageProfileThunk.pending, (prevState) => {
       return {
         ...prevState,
-        isLoading: true,
-        isFulfilled: false,
-        isRejected: false,
+        uploadImageProfile: {
+          isLoading: true,
+          isFulfilled: false,
+          isRejected: false,
+          data: null,
+          err: null,
+        },
       };
     });
     builder.addCase(
@@ -167,31 +238,38 @@ const userSlice = createSlice({
       (prevState, action: PayloadAction<any>) => {
         return {
           ...prevState,
-          isLoading: false,
-          isFulfilled: true,
-          isRejected: false,
-          uploadImageProfile: action.payload,
+          uploadImageProfile: {
+            isLoading: false,
+            isFulfilled: true,
+            isRejected: false,
+            data: action.payload,
+            err: null,
+          },
         };
       }
     );
-    builder.addCase(
-      uploadImageProfileThunk.rejected,
-      (prevState, action: PayloadAction<any>) => {
-        return {
-          ...prevState,
+    builder.addCase(uploadImageProfileThunk.rejected, (prevState, action) => {
+      return {
+        ...prevState,
+        uploadImageProfile: {
           isLoading: false,
           isFulfilled: false,
-          isRejected: false,
-          err: action.payload,
-        };
-      }
-    );
+          isRejected: true,
+          data: { msg: action.error.message },
+          err: action.error.message,
+        },
+      };
+    });
     builder.addCase(editNoTelpThunk.pending, (prevState) => {
       return {
         ...prevState,
-        isLoading: true,
-        isFulfilled: false,
-        isRejected: false,
+        editNoTelp: {
+          isLoading: true,
+          isFulfilled: false,
+          isRejected: false,
+          data: null,
+          err: null,
+        },
       };
     });
     builder.addCase(
@@ -199,25 +277,28 @@ const userSlice = createSlice({
       (prevState, action: PayloadAction<any>) => {
         return {
           ...prevState,
-          isLoading: false,
-          isFulfilled: true,
-          isRejected: false,
-          editNoTelp: action.payload,
+          editNoTelp: {
+            isLoading: false,
+            isFulfilled: true,
+            isRejected: false,
+            data: action.payload,
+            err: null,
+          },
         };
       }
     );
-    builder.addCase(
-      editNoTelpThunk.rejected,
-      (prevState, action: PayloadAction<any>) => {
-        return {
-          ...prevState,
+    builder.addCase(editNoTelpThunk.rejected, (prevState, action) => {
+      return {
+        ...prevState,
+        editProfile: {
           isLoading: false,
           isFulfilled: false,
-          isRejected: false,
-          err: action.payload,
-        };
-      }
-    );
+          isRejected: true,
+          data: { msg: action.error.message },
+          err: action.error.message,
+        },
+      };
+    });
   },
 });
 
