83cd589642a09b13af6db52144ed70e94f2e86cf
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-05-03 18:49:22 +0700

    Updated api

--- src/utils/api/subtasks.ts
+++ src/utils/api/subtasks.ts
@@ -2,8 +2,8 @@ import Axios from "axios";
 
 import {
   BodyArg,
-  ParamsArg,
-  IdTasksArg,
+  // ParamsArg,
+  // IdTasksArg,
   IdSubtasksArg,
 } from "../types/apiType";
 
@@ -17,14 +17,30 @@ const config = (accessToken: any) => {
   };
 };
 
-export const retriveSubtasks = (accessToken: any, params: ParamsArg, id: IdTasksArg) =>
-  Axios.get(`${BASE_URL}/subtasks/${id}?${params}`, config(accessToken));
+// export const retriveSubtasks = (
+//   accessToken: any,
+//   params: ParamsArg,
+//   taskId: IdTasksArg
+// ) => Axios.get(`${BASE_URL}/subtasks/${taskId}?${params}`, config(accessToken));
+
+export const retriveSubtasks = (
+  accessToken: any,
+) => Axios.get(`${BASE_URL}/subtasks`, config(accessToken));
 
 export const createSubtasks = (accessToken: any, body: BodyArg) =>
   Axios.post(`${BASE_URL}/subtasks/create`, body, config(accessToken));
 
-export const editSubtasks = (accessToken: any, body: BodyArg, id: IdSubtasksArg) =>
-  Axios.patch(`${BASE_URL}/subtasks/edit/${id}`, body, config(accessToken));
+export const editSubtasks = (
+  accessToken: any,
+  body: BodyArg,
+  id: IdSubtasksArg
+) => Axios.patch(`${BASE_URL}/subtasks/edit/${id}`, body, config(accessToken));
+
+export const editStatusSubtaks = (
+  accessToken: any,
+  body: BodyArg,
+  id: IdSubtasksArg
+) => Axios.patch(`${BASE_URL}/subtasks/edit/${id}`, body, config(accessToken));
 
 export const deleteSubtasks = (accessToken: any, id: IdSubtasksArg) =>
   Axios.delete(`${BASE_URL}/subtasks/delete/${id}`, config(accessToken));
