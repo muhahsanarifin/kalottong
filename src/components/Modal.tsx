e356e383c735337debe789ae42bc6aea848da0a7
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-10-30 23:31:56 +0700

    Delete border color.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -244,7 +244,7 @@ export const LogoutModal: React.FC<LogoutModalProps> = ({
   };
   return (
     <>
-      <div className=" flex fixed top-0 left-0 right-0 h-[100vh] flex-col items-center justify-center bg-transparant pt-4 z-50 border-2 border-solid border-green-500">
+      <div className=" flex fixed top-0 left-0 right-0 h-[100vh] flex-col items-center justify-center bg-transparant pt-4 z-50">
         <div className="fixed overflow-y-auto">
           <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
