fe32951611a583ebf70b040f86e0cc609b307fa8
MUH. AHSAN <muh.ahsan.student@gmail.com>
2023-10-30 23:04:13 +0700

    Delete footer import.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,6 @@ import type { NextPageWithLayout } from "../_app";
 import Title from "@/components/Layout";
 import Header from "@/components/Header";
 import Main from "@/components/Main";
-import Footer from "@/components/Footer";
 
 const Home: NextPageWithLayout = () => {
   return (
